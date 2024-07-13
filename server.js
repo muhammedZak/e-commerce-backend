2477325dc041d03153204830986955d358e82f7a
muhammedZak <zakariya.zaku018@gmail.com>
2024-07-13 20:13:54 +0530

    consoled

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = require('./app');
 const connectDB = require('./config/db');
+console.log('DB_URL:', process.env.DB_URL); // Add this line
+
 connectDB();
 
 const port = process.env.PORT || 4000;
