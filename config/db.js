98268bc2575c8a9b7dc5127f374083c1e1f29da9
muhammedZak <zakariya.zaku018@gmail.com>
2024-07-13 14:05:42 +0530

    bug

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const DB_PASS = process.env.DB_PASS;
-    const DB = process.env.DB_URL.replace('password', DB_PASS);
+    // const DB_PASS = process.env.DB_PASS;
+    const DB = process.env.DB_URL;
 
     const conn = await mongoose.connect(DB);
     console.log(`Mongodb connected on ${conn.connection.host}`);
