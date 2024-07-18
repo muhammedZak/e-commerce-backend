64bbe6e15c24f14937d32cd68feb37120da0457e
muhammedZak <zakariya.zaku018@gmail.com>
2024-07-18 15:25:12 +0530

    modified cookieoptions

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,7 @@ const createSendToken = (userId, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: 'None',
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
